bec202512c342e979066af296cde4817f5975b0d
usch143-svg <usch143@aucklanduni.ac.nz>
2026-08-26 14:56:55 +1200

    Update on understanding of the regulator

--- Uart testing/Uart testing/main.c
+++ Uart testing/Uart testing/main.c
@@ -13,23 +13,15 @@
 
 void USART_init(void)
 {
-	// Set baud
 	UBRR0H = (uint8_t)(UBRR_VALUE >> 8);
 	UBRR0L = (uint8_t)UBRR_VALUE;
-
-	// Enable transmit
 	UCSR0B = (1 << TXEN0);
-
-	// Asynchronous USART 8N1
 	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
 void USART_transmit(char data)
 {
-	// Wait for transmit buffer empty
 	while (!(UCSR0A & (1 << UDRE0)));
-
-	// Send data
 	UDR0 = data;
 }
 
@@ -41,65 +33,30 @@ void USART_string(const char *str)
 	}
 }
 
-void USART_uint(uint16_t value)
-{
-	char buffer[6];
-	uint8_t i = 0;
-
-	// Special case for 0
-	if (value == 0)
-	{
-		USART_transmit('0');
-		return;
-	}
-
-	// Convert integer into digits backwards
-	while (value > 0)
-	{
-		buffer[i++] = (value % 10) + '0';
-		value /= 10;
-	}
-
-	// Send digits in correct order
-	while (i > 0)
-	{
-		USART_transmit(buffer[--i]);
-	}
-}
-
 void print_RMSVoltage(double voltage)
 {
-	// Convert XX.X into integer tenths
 	uint16_t value = (uint16_t)(voltage * 10.0 + 0.5);
-
-	// Fixed extraction to get XX.X format
-	USART_transmit(((value / 100) % 10) + '0'); // Tens
-	USART_transmit(((value / 10) % 10) + '0');  // Ones
+	USART_transmit(((value / 100) % 10) + '0');
+	USART_transmit(((value / 10) % 10) + '0');
 	USART_transmit('.');
-	USART_transmit((value % 10) + '0');         // Tenths
+	USART_transmit((value % 10) + '0');
 }
 
 void print_PeakCurrent(double current)
 {
-	// Convert XXX into integer
 	uint16_t value = (uint16_t)(current + 0.5);
-
-	// Fixed extraction to get XXX format
-	USART_transmit(((value / 100) % 10) + '0'); // Hundreds
-	USART_transmit(((value / 10) % 10) + '0');  // Tens
-	USART_transmit((value % 10) + '0');         // Ones
+	USART_transmit(((value / 100) % 10) + '0');
+	USART_transmit(((value / 10) % 10) + '0');
+	USART_transmit((value % 10) + '0');
 }
 
 void print_Power(double power)
 {
-	// Convert X.XX into integer hundredths
 	uint16_t value = (uint16_t)(power * 100.0 + 0.5);
-
-	// Fixed extraction to get X.XX format
-	USART_transmit(((value / 100) % 10) + '0'); // Ones
+	USART_transmit(((value / 100) % 10) + '0');
 	USART_transmit('.');
-	USART_transmit(((value / 10) % 10) + '0');  // Tenths
-	USART_transmit((value % 10) + '0');         // Hundredths
+	USART_transmit(((value / 10) % 10) + '0');
+	USART_transmit((value % 10) + '0');
 }
 
 int main(void)
